952eb43dc1281a4495108dd683a0535479f1446b
Atena Hatta <108603260+AtenaHatta@users.noreply.github.com>
2022-11-13 14:46:54 -0800

    Add files via upload

---
+++ JavaFXLoginA/src/dbUtil/Config.java
@@ -0,0 +1,8 @@
+package dbUtil;
+
+public class Config {
+  
+  protected static final String URL = "jdbc:postgresql://db.qkkhpfdnanchbcwendpl.supabase.co:5432/postgres";
+  protected static final String USERNAME = "postgres";
+  protected static final String PASSWORD = "Ah19921102?16991699";
+}
